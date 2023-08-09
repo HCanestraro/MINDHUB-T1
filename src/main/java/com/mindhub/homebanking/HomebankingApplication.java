badcb92a66bc2d4a0fee3ea6aaa002a9e9665be5
HCanestraro <HCanestraro@users.noreply.github.com>
2023-08-08 23:50:52 -0300

    minhub tema 3 v1.00

--- src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -1,11 +1,15 @@
 package com.mindhub.homebanking;
 
 import com.mindhub.homebanking.Models.Client;
+import com.mindhub.homebanking.Models.Account;
 import com.mindhub.homebanking.Repositories.ClientRepository;
+import com.mindhub.homebanking.Repositories.AccountRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class HomebankingApplication {
@@ -15,15 +19,42 @@ public class HomebankingApplication {
 		SpringApplication.run(HomebankingApplication.class, args);
 	}
 	@Bean
-	public CommandLineRunner initData(ClientRepository clientRepository){
+	public CommandLineRunner initData(ClientRepository clientRepository, AccountRepository accountRepository){
 		return (args -> {
 			Client client1 = new Client();
 			client1.setFirstName("Javier");
 			client1.setLastName("Miller");
 			client1.setEmail("miller@mail.com");
 
-			clientRepository.save(client1);
+			Client client2 = new Client();
+			client2.setFirstName("Melba");
+			client2.setLastName("Moler");
+			client2.setEmail("melbar@minhub.com");
 
+			Account account1 = new Account();
+			account1.setNumber("VIN001");
+			account1.setCreationDate(LocalDateTime.now());
+			account1.setBalance(5000.0);
+			account1.setClient(client2);
+
+			Account account2 = new Account();
+			account2.setNumber("VIN002");
+			account2.setCreationDate( LocalDateTime.now().plusDays(1));
+			account2.setBalance(7500.0);
+			account2.setClient(client2);
+
+			Account account3 = new Account();
+			account3.setNumber("VIN003");
+			account3.setCreationDate( LocalDateTime.now().plusDays(5));
+			account3.setBalance(75000.0);
+			account3.setClient(client1);
+
+			clientRepository.save(client1);
+			clientRepository.save(client2);
+
+			accountRepository.save(account1);
+			accountRepository.save(account2);
+			accountRepository.save(account3);
 
 		});
 	}
--- src/main/java/com/mindhub/homebanking/Models/Account.java
+++ src/main/java/com/mindhub/homebanking/Models/Account.java
@@ -1,2 +1,59 @@
-package com.mindhub.homebanking.Models;public class Account {
+package com.mindhub.homebanking.Models;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime; // Import the LocalDateTime class
+import java.time.format.DateTimeFormatter; // Import the DateTimeFormatter class
+
+@Entity
+public class Account {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String number;
+	private LocalDateTime creationDate;
+	private double balance;
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "client_id")
+	private Client client;
+	public Account() {
+	}
+	public Account(String number, LocalDateTime creationDate, double balance, Client client) {
+		this.number = number;
+		this.creationDate = creationDate;
+		this.balance = balance;
+		this.client = client;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public LocalDateTime getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(LocalDateTime creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
 }
